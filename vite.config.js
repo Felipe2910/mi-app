30035c6536ea8f142c152393d41c74800e1c2879
Felipe Pool <fpoolcruz@gmail.com>
2025-08-06 02:57:25 -0600

    MiAppCoded

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import ColorThief from 'color-thief-browser';
 
 export default defineConfig({
-  base: '/mi-app/',
+  base: 'https://felipe2910.github.io/mi-app/',
   plugins: [react(), tailwindcss(), ColorThief()],
 })
