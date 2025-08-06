b00493a2cf14780f17833afcd9be8eb1efce5e40
Felipe Pool <fpoolcruz@gmail.com>
2025-08-06 01:35:45 -0600

    MiAppReCoded

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import ColorThief from 'color-thief-browser';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), ColorThief()],
-    server: {
-    host: true, // permite conexiones desde otras IPs (no solo localhost)
-    port: 5173, // puedes cambiar el puerto si lo deseas
-  },
-})
+  base: '/mi-app/',
+  plugins: [react(), tailwindcss(), ColorThief()]
+});
